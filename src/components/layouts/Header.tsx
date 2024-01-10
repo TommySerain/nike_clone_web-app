0dfb52ee9a28d1ba8f67cf909ca3e12c2849d4e8
TommySerain <tommyserain@yahoo.fr>
2024-01-10 16:40:15 +0100

    test fix

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,5 +1,5 @@
-import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
-import { HeartIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
+// import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
+// import { HeartIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 import MainNav from './MainNav'
 import Logo from './Logo'
@@ -17,13 +17,13 @@ function Header() {
                     <div className='flex w-60 bg-black/5 rounded-full space-x-4 py-2 pl-2 pr-8'>
                         {/* <div className='w-7 h-7 bg-black rounded-full'>
                         </div> */}
-                        <MagnifyingGlassIcon className="h-7"/>
+                        {/* <MagnifyingGlassIcon className="h-7"/> */}
                         <span className='text-black/20 font-bold'>
                             {`Rechercher`}
                         </span>
                     </div>
-                    <HeartIcon className="icon-btn"/>
-                    <ShoppingBagIcon className="icon-btn"/>
+                    {/* <HeartIcon className="icon-btn"/>
+                    <ShoppingBagIcon className="icon-btn"/> */}
                 </div>
             </div>
         </header>
